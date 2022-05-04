2234512b392777437df44ca7ec4f88388d443578
waltershitcoedr <67135647+ProYT303@users.noreply.github.com>
2022-05-04 18:35:05 +0700

    Update QQBacklist.java

--- src/main/java/net/ccbluex/liquidbounce/features/worker/QQBacklist.java
+++ src/main/java/net/ccbluex/liquidbounce/features/worker/QQBacklist.java
@@ -8,48 +8,10 @@ import java.util.List;
 
 public class QQBacklist {
     public static List<String> backlist=new ArrayList<String>();
-    public static void doCheck(){
-        for(String line:backlist){
-            if(line.length()==QQUtils.QQNumber.length()){
-                if(line.contains(QQUtils.QQNumber)){
-                    System.out.println("QQ Verify failed!");
-                    try {
-                        if (SystemTray.isSupported()) {
-                            TrayIcon trayIcon = new TrayIcon(Toolkit.getDefaultToolkit().createImage("icon.png"), "FDPProtect Verify failed");
-                            trayIcon.setImageAutoSize(true);
-                            SystemTray.getSystemTray().add(trayIcon);
-                            trayIcon.displayMessage("Verify failed", "You are backlist user!Take action: do nothing", TrayIcon.MessageType.ERROR);
-                            Thread.sleep(5000);
-                            SystemTray.getSystemTray().remove(trayIcon);
-                        }
-                    } catch (Exception E) {
-                        E.printStackTrace();
-                    }
-                }
-            }
-        }
+    public static void doCheck() {
     }
 
     public static void doCheckByProtect(String str){
-        String[] lines=str.split("#");
-        for(String line:backlist){
-            if(line.length()==QQUtils.QQNumber.length()){
-                if(line.contains(QQUtils.QQNumber)){
-                    System.out.println("FDPProtect verify failed!");
-                    try {
-                        if (SystemTray.isSupported()) {
-                            TrayIcon trayIcon = new TrayIcon(Toolkit.getDefaultToolkit().createImage("icon.png"), "FDPProtect Verify failed");
-                            trayIcon.setImageAutoSize(true);
-                            SystemTray.getSystemTray().add(trayIcon);
-                            trayIcon.displayMessage("FDPProtect Verify failed", "You are backlist user!Take action: do nothing", TrayIcon.MessageType.ERROR);
-                            Thread.sleep(5000);
-                            SystemTray.getSystemTray().remove(trayIcon);
-                        }
-                    } catch (Exception E) {
-                        E.printStackTrace();
-                    }
-                }
-            }
-        }
+        
     }
 }
