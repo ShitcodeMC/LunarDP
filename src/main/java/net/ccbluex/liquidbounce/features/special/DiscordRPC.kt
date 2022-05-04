6e7c4f12c50e4c06d0004e0e7f9df5871cea509d
waltershitcoedr <67135647+ProYT303@users.noreply.github.com>
2022-05-04 18:32:42 +0700

    Add files via upload

--- src/main/java/net/ccbluex/liquidbounce/features/special/DiscordRPC.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/special/DiscordRPC.kt
@@ -5,16 +5,17 @@ import com.jagrosh.discordipc.IPCListener
 import com.jagrosh.discordipc.entities.RichPresence
 import com.jagrosh.discordipc.entities.pipe.PipeStatus
 import net.ccbluex.liquidbounce.LiquidBounce
+import net.ccbluex.liquidbounce.LiquidBounce.CLIENT_VERSION
 import net.ccbluex.liquidbounce.utils.ServerUtils
 import org.json.JSONObject
 import java.time.OffsetDateTime
 import kotlin.concurrent.thread
 
 object DiscordRPC {
-    private val ipcClient = IPCClient(871606857981128724)
+    var ipcClient = IPCClient(836549947574321172)
     private val timestamp = OffsetDateTime.now()
-    private var running = false
-    private var fdpwebsite = "fdpclient.club - "
+    var running = false
+	var image = "lunar"
 
     fun run() {
         ipcClient.setListener(object : IPCListener {
@@ -41,12 +42,11 @@ object DiscordRPC {
     private fun update() {
         val builder = RichPresence.Builder()
         builder.setStartTimestamp(timestamp)
-        builder.setLargeImage("cfb8fe2fe9169dc68f7f8c1236b885")
-        builder.setDetails(fdpwebsite + LiquidBounce.CLIENT_VERSION)
-        ServerUtils.getRemoteIp().also {
-            builder.setState(if(it.equals("idling", true)) "Idling" else "Server: $it ")
+        builder.setLargeImage(image)
+		ServerUtils.getRemoteIp().also {
+            builder.setState(if(it.equals("idling", true)) "Doing nothing" else "BHopping on $it with LunarDP $CLIENT_VERSION")
         }
-
+		builder.setDetails("With minecraft old_alpha rd-132211")
         // Check ipc client is connected and send rpc
         if (ipcClient.status == PipeStatus.CONNECTED)
             ipcClient.sendRichPresence(builder.build())
